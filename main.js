e65b99d98c50cf75eaa003584963d8f9af1b851a
AlipTS <110875205+AlipTS@users.noreply.github.com>
2023-12-16 00:29:32 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('.section');
+
+    function checkVisible() {
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+            const isVisible = (rect.top >= 0 && rect.bottom <= window.innerHeight);
+
+            if (isVisible) {
+                section.classList.add('visible');
+            }
+        });
+    }
+
+    document.addEventListener('scroll', checkVisible);
+    document.addEventListener('resize', checkVisible);
+
+    // Panggil fungsi setelah halaman dimuat
+    checkVisible();
+});
