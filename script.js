125f7fe69fc18af884b83a74438a198b9f5b3527
AlipTS <110875205+AlipTS@users.noreply.github.com>
2023-12-16 00:39:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const sections = document.querySelectorAll('section');
 
-    function checkVisibility() {
-        sections.forEach(section => {
-            const bounding = section.getBoundingClientRect();
-            if (
-                bounding.top >= 0 &&
-                bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-            ) {
-                section.style.opacity = '1';
-            } else {
-                section.style.opacity = '0';
-            }
-        });
-    }
+//     function checkVisibility() {
+//         sections.forEach(section => {
+//             const bounding = section.getBoundingClientRect();
+//             if (
+//                 bounding.top >= 0 &&
+//                 bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+//             ) {
+//                 section.style.opacity = '1';
+//             } else {
+//                 section.style.opacity = '0';
+//             }
+//         });
+//     }
 
-    window.addEventListener('scroll', checkVisibility);
-    window.addEventListener('resize', checkVisibility);
+//     window.addEventListener('scroll', checkVisibility);
+//     window.addEventListener('resize', checkVisibility);
 
-    // Memastikan efek transisi diterapkan saat halaman dimuat
-    checkVisibility();
-});
+//     // Memastikan efek transisi diterapkan saat halaman dimuat
+//     checkVisibility();
+// });
 
